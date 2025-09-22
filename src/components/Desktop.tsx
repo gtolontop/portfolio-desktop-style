d7b14f538a9c98d78d8adf847868dff4613784df
Cruck Store <cruckstore+github@gmail.com>
2025-09-22 22:30:00 +0200

    Add Taskbar styles and integrate Taskbar component into Desktop

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -1,9 +1,12 @@
 'use client'
 
+import Taskbar from './Taskbar'
+
 export default function Desktop() {
   return (
     <div className="desktop-container">
       {/* Desktop content will go here */}
+      <Taskbar />
     </div>
   )
 }
