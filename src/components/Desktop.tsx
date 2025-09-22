f2c421dd586d19e524d7be9bcc83c03d5a4dd8ce
Cruck Store <cruckstore+github@gmail.com>
2025-09-22 22:37:09 +0200

    Remove texture overlay from Desktop component background

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -4,7 +4,7 @@ import Taskbar from './Taskbar'
 
 export default function Desktop() {
   return (
-    <div className="w-screen h-screen relative overflow-hidden bg-gradient-to-br from-pink-200 via-pink-100 to-pink-50">
+    <div className="w-screen h-screen relative overflow-hidden bg-gradient-to-br from-pink-200 to-pink-50">
       {/* Texture overlay */}
       <div 
         className="absolute inset-0 opacity-[0.03]"
