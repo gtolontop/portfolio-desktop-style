d3911ea2d0953f20ea2271498618317b2b0ae0d3
Cruck Store <cruckstore+github@gmail.com>
2025-09-22 22:37:47 +0200

    Update background color of Desktop component

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -4,7 +4,7 @@ import Taskbar from './Taskbar'
 
 export default function Desktop() {
   return (
-    <div className="w-screen h-screen relative overflow-hidden bg-pink-200">
+    <div className="w-screen h-screen relative overflow-hidden bg-pink-300">
       {/* Texture overlay */}
       <div 
         className="absolute inset-0 opacity-[0.03]"
