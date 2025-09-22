1239d932f596eac9fce5121bca1dbcad9f35b073
Cruck Store <cruckstore+github@gmail.com>
2025-09-22 22:25:19 +0200

    Add Desktop component with initial layout

---
+++ src/components/Desktop.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Desktop() {
+  return (
+    <div className="w-full h-screen bg-blue-500 relative overflow-hidden">
+      {/* Desktop content will go here */}
+    </div>
+  )
+}
