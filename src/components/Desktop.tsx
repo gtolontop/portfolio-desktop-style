a6764483cf2b9c368b26bbd801fea00b5dc464ce
Cruck Store <cruckstore+github@gmail.com>
2025-09-22 22:28:01 +0200

    Refactor Desktop component to use a specific class for styling

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -2,7 +2,7 @@
 
 export default function Desktop() {
   return (
-    <div className="w-full h-screen bg-blue-500 relative overflow-hidden">
+    <div className="desktop-container">
       {/* Desktop content will go here */}
     </div>
   )
