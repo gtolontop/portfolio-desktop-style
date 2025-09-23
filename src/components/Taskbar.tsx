af9b762057e48c6383cd13fa19a50f76a2ad5bc4
Cruck Store <cruckstore+github@gmail.com>
2025-09-23 20:36:07 +0200

    Update Taskbar home icon size for improved scaling

--- src/components/Taskbar.tsx
+++ src/components/Taskbar.tsx
@@ -55,7 +55,7 @@ export default function Taskbar() {
           <img
             src="/images/home.png"
             alt="Start"
-            className="w-10 h-10 object-contain"
+            className="w-7 h-7 object-contain"
           />
         </button>
 
